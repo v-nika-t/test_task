06d8e7f4f8240e12302b4ae88d2c03311e3f9ba4
Tsishalovich Veranika <vt3199662@gmail.com>
2023-01-23 17:51:09 +0300

    created method get changed elemts and show elements array

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+string[] family = {"mother", "father", "son", "daughter", "dog", "cat"};
+
+string [] GetElementsByParams(string [] array, int countSymbols = 4) {
+       string[] changedArray = new string[array.Length];
+       int k = 0;
+        for(int i = 0; i < family.Length; i++) {
+            if(array[i].Length < countSymbols) {
+                changedArray[k++] = family[i];  
+            }
+        }
+    return changedArray;
+}
+
+
+void ShowArray(string [] array) {
+        for(int i = 0; i < array.Length; i++) {
+            if(!String.IsNullOrEmpty(array[i])) Console.WriteLine($"{array[i]}");
+        }
+}
+
+ShowArray(GetElementsByParams(family));
